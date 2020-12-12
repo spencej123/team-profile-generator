4cd654f28ff2d89a2bf757efc168887c1e2f0f0f
Spencer Johnson <spencer.johnson123@hotmail.com>
2020-12-11 20:34:46 -0600

    working on the other objects

--- app.js
+++ app.js
@@ -52,3 +52,68 @@ const managerQuestions = [
     choices: ["Yes", "No"],
   },
 ];
+
+const employeeQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter employee name:",
+    validate: async (input) => {
+      if (input == "") {
+        return "Please enter a name.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter their email:",
+    validate: async (input) => {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+        return true;
+      }
+      return "Please enter a valid email address.";
+    },
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is their role?",
+    choices: ["engineer", "intern"],
+  },
+  {
+    when: (input) => {
+      return input.role == "engineer";
+    },
+    type: "input",
+    name: "github",
+    message: "Engineer, enter your github username:",
+    validate: async (input) => {
+      if (input == "" || /\s/.test(input)) {
+        return "Please enter a valid GitHub username";
+      }
+      return true;
+    },
+  },
+  {
+    when: (input) => {
+      return input.role == "intern";
+    },
+    type: "input",
+    name: "school",
+    message: "Intern, enter your school name:",
+    validate: async (input) => {
+      if (input == "") {
+        return "Please enter a name.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "addAnother",
+    message: "Add another team member?",
+    choices: ["Yes", "No"],
+  },
+];
